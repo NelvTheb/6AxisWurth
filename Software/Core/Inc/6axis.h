a322766be16015d6b326908f154f2c501240d19c
Nelven <nelven.thebault@gmail.com>
2024-11-12 14:30:49 +0100

    C and H

---
+++ Software/Core/Inc/6axis.h
@@ -0,0 +1,13 @@
+/*
+ * 6axis.h
+ *
+ *  Created on: Nov 12, 2024
+ *      Author: nelven
+ */
+
+#ifndef INC_6AXIS_H_
+#define INC_6AXIS_H_
+
+
+
+#endif /* INC_6AXIS_H_ */
---
+++ Software/Core/Src/6axis.c
@@ -0,0 +1,8 @@
+/*
+ * 6axis.c
+ *
+ *  Created on: Nov 12, 2024
+ *      Author: nelven
+ */
+
+
